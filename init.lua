ba31c4857f8f96c4180e4f9f6e07734feb191e85
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-02-28 23:06:52 -0600

    Init commit :rocket:

---
+++ init.lua
@@ -0,0 +1,10 @@
+local vim = vim
+
+-- config option initialization
+require('options')
+
+-- plugin initialization
+require('plugin.plugins')
+
+-- color scheme initialization
+vim.cmd('colorscheme kanagawa-wave')
---
+++ plugin/plugins.lua
@@ -0,0 +1,9 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- theme
+Plug('rebelot/kanagawa.nvim')
+
+vim.call('plug#end')
